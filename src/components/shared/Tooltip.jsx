418e30b079566049031f453f2088799da081e3d3
AussieScorcher <edward@stopbars.com>
2025-11-21 22:00:00 +0800

    feat(Tooltip): add Tooltip component for enhanced UI interactions

---
+++ src/components/shared/Tooltip.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+export const Tooltip = ({ children, content, className = '' }) => {
+  if (!content) return children;
+
+  return (
+    <div className={`relative flex items-center group ${className}`}>
+      {children}
+      <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-1.5 bg-zinc-900 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap border border-zinc-700 z-50 shadow-lg">
+        {content}
+        {/* Arrow */}
+        <div className="absolute top-full left-1/2 transform -translate-x-1/2 -mt-1 border-r border-b border-zinc-700 bg-zinc-900 w-2 h-2 rotate-45" />
+      </div>
+    </div>
+  );
+};
+
+Tooltip.propTypes = {
+  children: PropTypes.node.isRequired,
+  content: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+  className: PropTypes.string,
+};
